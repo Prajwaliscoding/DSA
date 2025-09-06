829f398bf1f70552b246f8a087c6605cee91233e
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-09-06 13:36:44 -0500

    Corrected the minor error in binarySearch.c

--- Binary-Search/binarySearch.c
+++ Binary-Search/binarySearch.c
@@ -12,15 +12,15 @@ int binarySearch(int arr[], int n, int target)
         {
             return mid;
         }
-        else if (arr[mid] < target)
+
+        if (arr[mid] < target)
         {
             left = mid + 1;
         }
-        else
-            (arr[mid] > target)
-            {
-                right = mid - 1;
-            }
+        else if (arr[mid] > target)
+        {
+            right = mid - 1;
+        }
     }
     return -1;
 }
